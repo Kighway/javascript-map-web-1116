1a008428e7f3ed61ad1bb7aa0a67cc78a3840f9d
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-01-25 16:19:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+
+// In case we ever need the index of the item or the full list of items in our callback function, let's add these as arguments to our callback. The callback doesn't have to use these values, but they're there if we ever need them.
+
+function map(collection, callback) {
+  const result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element, i, collection));
+  }
+
+  return result;
+}
